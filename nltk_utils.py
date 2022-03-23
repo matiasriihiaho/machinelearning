044f1a56842bafbc6b836b40af3b98eae7e2c9b8
matiasriihiaho <102151774+matiasriihiaho@users.noreply.github.com>
2022-03-23 13:19:09 +0200

    Delete nltk_utils.py

--- nltk_utils.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-from textblob import TextBlob
-cmd = ['python3','-m','textblob.download_corpora']
-#cmd = 'python -m textblob.download_corpora'
-subprocess.run(cmd)
-print("Working")
