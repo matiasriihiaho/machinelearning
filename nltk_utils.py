61ef5c1e01b2344bbf78be10e144f7f6b682f67d
matiasriihiaho <102151774+matiasriihiaho@users.noreply.github.com>
2022-03-23 11:33:20 +0200

    Add files via upload

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,4 +1,5 @@
 import subprocess
-cmd = ['python3','-m','textblob.download_corpora']
+#cmd = ['python3','-m','textblob.download_corpora']
+cmd = 'python -m textblob.download_corpora'
 subprocess.run(cmd)
 print("Working")
